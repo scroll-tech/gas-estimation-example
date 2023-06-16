3fd6085f29255817f05f021bc6bef985ec672e27
Filip Pajic <fica.pajic@gmail.com>
2023-06-16 19:27:56 +0200

    Added Oracle helper transactions

---
+++ scripts/helpers/oracle.ts
@@ -0,0 +1,20 @@
+import {ethers} from "hardhat";
+import { ContractTransaction } from "ethers";
+
+export async function estimateL1DataFee(gasOraclePrecompileAddress: string, unsignedSerializedTransaction: string): Promise<bigint> {
+  const l1GasOracle = await ethers.getContractAt("IL1GasPriceOracle", gasOraclePrecompileAddress);
+
+  return l1GasOracle.getL1GasUsed(unsignedSerializedTransaction);
+}
+
+export async function estimateL2ExecutionFee(tx: ContractTransaction): Promise<bigint> {
+  const gasToUse = await ethers.provider.estimateGas(tx);
+  const feeData = await ethers.provider.getFeeData();
+  const gasPrice = feeData.gasPrice;
+
+  if (!gasPrice) {
+    throw new Error("There was an error estimating L2 execution cost");
+  }
+
+  return gasToUse * gasPrice;
+}
